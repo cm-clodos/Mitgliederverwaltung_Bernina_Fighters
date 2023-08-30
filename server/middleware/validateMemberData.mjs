ee718eb39f07c08b92684eab004ff09df22a4542
clodos <clodos_m92@hotmail.com>
2023-08-30 14:40:48 +0200

    refactor von errorChecks

--- server/middleware/validateMemberData.mjs
+++ server/middleware/validateMemberData.mjs
@@ -10,10 +10,10 @@ import {
 } from "../services/FieldChecker.mjs";
 
 export function validateMemberData(req, res, next) {
+
     req.body = trimData(req.body);
     req.body.firstname = formatFirstLetterOfNames(req.body.firstname)
     req.body.lastname = formatFirstLetterOfNames(req.body.lastname)
-
     const {firstname, lastname, email, telephone, active, role_id, entry_date} = req.body;
 
     const errors = []
@@ -25,13 +25,21 @@ export function validateMemberData(req, res, next) {
     const roleIdError = checkRoleId(role_id);
     const entryDateError = checkEntryDate(entry_date);
 
-    if (Object.keys(firstnameError).length > 0) errors.push(firstnameError);
-    if (Object.keys(lastnameError).length > 0) errors.push(lastnameError);
-    if (Object.keys(emailError).length > 0) errors.push(emailError);
-    if (Object.keys(telephoneError).length > 0) errors.push(telephoneError);
-    if (Object.keys(activeError).length > 0) errors.push(activeError);
-    if (Object.keys(roleIdError).length > 0) errors.push(roleIdError);
-    if (Object.keys(entryDateError).length > 0) errors.push(entryDateError);
+    const errorChecks = [
+        { error: firstnameError, key: 'firstname' },
+        { error: lastnameError, key: 'lastname' },
+        { error: emailError, key: 'email' },
+        { error: telephoneError, key: 'telephone' },
+        { error: activeError, key: 'active' },
+        { error: roleIdError, key: 'role_id' },
+        { error: entryDateError, key: 'entry_date' }
+    ];
+
+    errorChecks.forEach(({ error, key }) => {
+        if (Object.keys(error).length > 0) {
+            errors.push({ key, error });
+        }
+    });
 
     if (errors.length > 0) {
         res.status(400).json(errors);
