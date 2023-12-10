d1ecd1307c98c6f1c483db1843cdc79a0b3b1da8
clodos <clodos_m92@hotmail.com>
2023-12-10 11:56:09 +0100

    loginView als Startseite

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+    <main class="login">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 mt-5">
+                    <div class="card">
+                        <div class="card-header bg-primary text-white">
+                            <h3 class="card-title text-center">Login</h3>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="text" class="form-control" id="email" name="email" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Passwort</label>
+                                    <input type="password" class="form-control" id="password" name="password" required>
+                                </div>
+                                <div class="d-grid">
+                                    <button type="submit" class="btn btn-primary">Anmelden</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
