9ff33e6680ae51318348f164a3173a6270f52b14
clodos <clodos_m92@hotmail.com>
2024-05-01 11:14:57 +0200

    Liste wird nach Periode gefiltert auch nach Update

--- client/src/views/MemberPaymentView.vue
+++ client/src/views/MemberPaymentView.vue
@@ -5,43 +5,43 @@
       <div class="card">
         <div class="card-header">
           <h4>Bezahlperiode: <select id="period" v-model="selectedPeriod" @change="renderSelectedPaymentPeriod">
-            <option value="">Jahr auswählen</option>
-            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
-          </select>
-            <button type="button" @click="confirmation" class="btn btn-primary float-end">Bezahlperiode erstellen</button>
+              <option value="">Jahr auswählen</option>
+              <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
+            </select>
+            <button type="button" @click="confirmation" class="btn btn-primary float-end">Bezahlperiode
+              erstellen</button>
           </h4>
         </div>
         <div class="card-body">
           <table class="table table-bordered">
             <thead>
-            <tr>
-              <th>Vorname</th>
-              <th>Nachname</th>
-              <th @click="sortByPaid">Bezahlt <font-awesome-icon icon="sort"/></th>
-              <th>Bezahldatum</th>
-              <th @click="sortByDate">Periode <font-awesome-icon icon="sort"/></th>
-            </tr>
+              <tr>
+                <th>Vorname</th>
+                <th>Nachname</th>
+                <th @click="sortByPaid">Bezahlt <font-awesome-icon icon="sort" /></th>
+                <th>Bezahldatum</th>
+                <th @click="sortByDate">Periode <font-awesome-icon icon="sort" /></th>
+              </tr>
             </thead>
             <tbody v-if="this.filteredPayments.length > 0">
-            <tr v-for="(payment, index) in this.filteredPayments" :key="index">
-              <td data-cell="vorname"> {{payment.firstname}}</td>
-              <td data-cell="nachname"> {{payment.lastname}}</td>
-              <td data-cell="bezahlt"><input type="checkbox" @change="updatePayment(payment.id, payment.paid)" v-model="payment.paid" :checked="payment.paid === 1" :value="1"></td>
-              <td data-cell="bezahldatum"> {{payment.paid_date ? formatDate(payment.paid_date) : '' }}</td>
-              <td data-cell="periode"> {{formatDate(payment.created_at)}}</td>
-            </tr>
+              <tr v-for="(payment, index) in this.filteredPayments" :key="index">
+                <td data-cell="vorname"> {{ payment.firstname }}</td>
+                <td data-cell="nachname"> {{ payment.lastname }}</td>
+                <td data-cell="bezahlt"><input type="checkbox" @change="updatePayment(payment.id, payment.paid)"
+                    v-model="payment.paid" :checked="payment.paid === 1" :value="1"></td>
+                <td data-cell="bezahldatum"> {{ payment.paid_date ? formatDate(payment.paid_date) : '' }}</td>
+                <td data-cell="periode"> {{ formatDate(payment.created_at) }}</td>
+              </tr>
             </tbody>
-            <tbody v-else >
-            <tr>
-              <td colspan="8" class="text-center">Daten werden geladen...</td>
-            </tr>
+            <tbody v-else>
+              <tr>
+                <td colspan="8" class="text-center">Daten werden geladen...</td>
+              </tr>
             </tbody>
           </table>
-          <ConfirmModal :show="modalVisible"
-                        @confirm="handleConfirm"
-                        @cancel="closeModal"
-                        title="Bezahlperiode erstellen"
-                        message="Sind Sie sicher, dass Sie eine neue Bezahlperiode erstellen möchten?"></ConfirmModal>
+          <ConfirmModal :show="modalVisible" @confirm="handleConfirm" @cancel="closeModal"
+            title="Bezahlperiode erstellen"
+            message="Sind Sie sicher, dass Sie eine neue Bezahlperiode erstellen möchten?"></ConfirmModal>
         </div>
       </div>
     </div>
@@ -50,9 +50,9 @@
 
 <script>
 import axios from "/src/api/axios.mjs";
-import {formatInSwissTime} from "/src/services/formatterService.mjs";
-import {useToast} from 'vue-toast-notification';
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { formatInSwissTime } from "/src/services/formatterService.mjs";
+import { useToast } from 'vue-toast-notification';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ConfirmModal from "@/components/ConfirmModal.vue";
 
 export default {
@@ -95,24 +95,25 @@ export default {
           paid: payment.paid === 1,
         }))
         this.filteredPayments = this.formattedPayments
+        this.renderSelectedPaymentPeriod();
       })
     },
     updatePayment(id, paidStatus) {
-        axios.put(`/members/payments/${id}`, {
-          paid: paidStatus
-        }).then(res => {
-          if (res.status === 200) {
-            this.toast.success(res.data.message);
-            this.getPayments();
-          }
-        }).catch(error => {
+      axios.put(`/members/payments/${id}`, {
+        paid: paidStatus
+      }).then(res => {
+        if (res.status === 200) {
+          this.toast.success(res.data.message);
+          this.getPayments();
+        }
+      }).catch(error => {
         console.log(error);
         if ([404, 500].includes(error.response.status)) {
           this.toast.error(error.response.data.message);
         } else {
           console.log("Unexpected error: " + error.response.status);
         }
-        });
+      });
     },
     formatDate(date) {
       return formatInSwissTime(date);
@@ -136,7 +137,7 @@ export default {
         this.years.push(year);
       }
     },
-    createNewPaymentPeriod(){
+    createNewPaymentPeriod() {
       axios.post("/members/payments/period").then(res => {
         if (res.status === 201) {
           this.toast.success(res.data.message);
@@ -166,7 +167,7 @@ export default {
     handleConfirm(value) {
       this.modalVisible = false;
       if (value) {
-      this.createNewPaymentPeriod();
+        this.createNewPaymentPeriod();
       }
     },
     closeModal() {
@@ -179,6 +180,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
