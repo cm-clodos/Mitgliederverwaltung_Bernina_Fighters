d15022cc57da51fbf4640b9cedbf1bcba14cab49
clodos <clodos_m92@hotmail.com>
2024-05-21 09:17:11 +0200

    Table Categories Uniquekey hinzugefügt + Reihenfolge geändert

--- sql/tables/06a_transCategories.sql
+++ sql/tables/06a_transCategories.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `transCategories` (
     `id` int(11) PRIMARY KEY AUTO_INCREMENT,
     `name` varchar(50) NOT NULL,
-    `type` ENUM('Einnahme', 'Ausgabe') NOT NULL
+     UNIQUE KEY `name` (`name`)
 );
