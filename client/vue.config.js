86a16ba6696cab38b3686112155bd2f84241df87
clodos <clodos_m92@hotmail.com>
2024-05-31 16:24:33 +0200

    Proxy Fehler behoben

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    //server: defineServerConfiguration(),
-    proxy: process.env.VUE_APP_API_URI,
-  },
+    transpileDependencies: true,
+    devServer: {
+        //server: defineServerConfiguration(),
+        //proxy: process.env.VUE_APP_API_URI,
+        proxy: {
+            // Allgemeine API-Proxy-Regel
+            "^/": {
+                target: process.env.VUE_APP_API_URI,
+                changeOrigin: true,
+                ws: false, // Deaktivieren von WebSocket-Proxys
+            },
+        },
+    },
 });
